2508cc99d2c7e620ad700d4bb300f6cf93cf3dd1
Cassandra Parsons <parsons0006@gmail.com>
2021-02-17 17:59:24 -0500

    practice work_resolving promises

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
+const question = "Will the weather be nice today?";
+const promise = tell(question);
+
+promise
+  .then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  })
+  .catch(console.log);
+
+welcome()
+  .then(console.log)
+  .then(() => {
+    goodbye().then(console.log);
+  });
+
+/*
+promise.then((result) => {
+  console.log(result);
+});
+//promise.then(console.log);
+
+
 const promise = welcome();
 
 console.log(promise);
+*/
